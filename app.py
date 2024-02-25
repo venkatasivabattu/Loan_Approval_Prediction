552286fcc027af7bd3e5f3e94b56e45fb8df8347
venkatasivabattu <venkatasivabattu2002@gmail.com>
2024-02-25 12:09:13 +0530

    'kk'

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import numpy as np
 import joblib
 import pandas as pd
+import requests
 
 app = Flask(__name__)
 @app.route('/')
@@ -15,44 +16,47 @@ def fun(x):
     l+=x[3:]
     d = pd.DataFrame([l], columns=['ApplicantIncome', 'LoanAmount', 'Married', 'Property_Area_Rural', 'Property_Area_Semiurban', 'Property_Area_Urban'])
     return m.predict(d)[0]
-def myfun():
-    df=pd.read_csv('static/train.csv')
-    df['Gender'].fillna(df['Gender'].mode()[0],inplace=True)
-    df['Married'].fillna(df['Married'].mode()[0],inplace=True)
-    df['Dependents'].fillna(df['Dependents'].mode()[0],inplace=True)
-    df['Self_Employed'].fillna(df['Self_Employed'].mode()[0],inplace=True)
-    df['Credit_History'].fillna(df['Credit_History'].mode()[0],inplace=True)
-    df['Loan_Amount_Term'].fillna(df['Loan_Amount_Term'].mode()[0],inplace=True)
-    df['LoanAmount'].fillna(df['LoanAmount'].mean(),inplace=True)
-    df = df.drop(['Loan_ID'], axis = 1)
-    df = pd.get_dummies(df)
-    df = df.drop(['Gender_Female', 'Married_No', 'Education_Not Graduate',
-                'Self_Employed_No', 'Loan_Status_N'], axis = 1)
-    new = {'Gender_Male': 'Gender', 'Married_Yes': 'Married',
-        'Education_Graduate': 'Education', 'Self_Employed_Yes': 'Self_Employed',
-        'Loan_Status_Y': 'Loan_Status'}
 
-    df.rename(columns=new, inplace=True)
-    print(df.head())
+# def myfun():
+#     df=pd.read_csv('static/train.csv')
+#     df['Gender'].fillna(df['Gender'].mode()[0],inplace=True)
+#     df['Married'].fillna(df['Married'].mode()[0],inplace=True)
+#     df['Dependents'].fillna(df['Dependents'].mode()[0],inplace=True)
+#     df['Self_Employed'].fillna(df['Self_Employed'].mode()[0],inplace=True)
+#     df['Credit_History'].fillna(df['Credit_History'].mode()[0],inplace=True)
+#     df['Loan_Amount_Term'].fillna(df['Loan_Amount_Term'].mode()[0],inplace=True)
+#     df['LoanAmount'].fillna(df['LoanAmount'].mean(),inplace=True)
+#     df = df.drop(['Loan_ID'], axis = 1)
+#     df = pd.get_dummies(df)
+#     df = df.drop(['Gender_Female', 'Married_No', 'Education_Not Graduate',
+#                 'Self_Employed_No', 'Loan_Status_N'], axis = 1)
+#     new = {'Gender_Male': 'Gender', 'Married_Yes': 'Married',
+#         'Education_Graduate': 'Education', 'Self_Employed_Yes': 'Self_Employed',
+#         'Loan_Status_Y': 'Loan_Status'}
+
+#     df.rename(columns=new, inplace=True)
+#     print(df.head())
     
-    ddf=df['Loan_Status']
-    df.ApplicantIncome = np.sqrt(df.ApplicantIncome)
-    df.CoapplicantIncome = np.sqrt(df.CoapplicantIncome)
-    df.LoanAmount = np.sqrt(df.LoanAmount)
-    df=df[['ApplicantIncome', 'LoanAmount', 'Married', 'Property_Area_Rural',
-       'Property_Area_Semiurban', 'Property_Area_Urban']]
-    print(df.head())
-    res=[]
-    for i in range(len(df)):
-        d = pd.DataFrame([df.iloc[i]], columns=['ApplicantIncome', 'LoanAmount', 'Married', 'Property_Area_Rural', 'Property_Area_Semiurban', 'Property_Area_Urban'])
-        res.append(m.predict(d)[0])
-    c=0
-    for i,j in zip(ddf,res):
-        if i==j:
-            c+=1
-    print(len(ddf),len(res),c)
-@app.route('/form', methods=['GET','POST'])
+#     ddf=df['Loan_Status']
+#     df.ApplicantIncome = np.sqrt(df.ApplicantIncome)
+#     df.CoapplicantIncome = np.sqrt(df.CoapplicantIncome)
+#     df.LoanAmount = np.sqrt(df.LoanAmount)
+#     df=df[['ApplicantIncome', 'LoanAmount', 'Married', 'Property_Area_Rural',
+#        'Property_Area_Semiurban', 'Property_Area_Urban']]
+#     print(df.head())
+#     res=[]
+#     for i in range(len(df)):
+#         d = pd.DataFrame([df.iloc[i]], columns=['ApplicantIncome', 'LoanAmount', 'Married', 'Property_Area_Rural', 'Property_Area_Semiurban', 'Property_Area_Urban'])
+#         res.append(m.predict(d)[0])
+#     c=0
+#     for i,j in zip(ddf,res):
+#         if i==j:
+#             c+=1
+#     print(len(ddf),len(res),c)
 
+
+
+@app.route('/form', methods=['GET','POST'])
 def form():
     if request.method == 'POST':
         id = request.form['id'].strip()
@@ -109,8 +113,7 @@ def form():
 
 @app.route('/result')
 def result(x,id,name):
-    return render_template('result.html',x=x,id=id,name=name.capitalize())
+    return render_template('result.html',x=0,id=id,name=name.capitalize())
 if __name__ == '__main__':
-    m=joblib.load('mymodel.pkl')
-    
+    m=joblib.load('mymodel_ensemble.pkl')
     app.run(debug=True)
